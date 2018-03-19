ffe17e82637e32bc3e34f97321971dc3581da31b
javakang <javakang@163.com>
2018-03-19 18:09:20 +0800

    init

---
+++ kspcloud-enreka/src/main/java/com/kspcloud/eureka/KspcloudEnrekaApplication.java
@@ -0,0 +1,14 @@
+package com.kspcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class KspcloudEnrekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KspcloudEnrekaApplication.class, args);
+	}
+}
